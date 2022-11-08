bbf7c2541ae2b0457e9757431c7941b761dac918
イオリ <i.takahashi.sys22@morijyobi.ac.jp>
2022-11-08 10:43:32 +0900

    java̕ҏW

--- main.java
+++ main.java
@@ -15,9 +15,22 @@ public class main {
 		System.out.println("体重:");
 		double b = sc2.nextDouble();
 
+		scanner sc3 = new Scanner(System.in);
+
+		System.out.println("二人目の身長:");
+		double c = sc3.nextDouble();
+
+		Scanner sc4 = new Scanner(System.in);
+
+		System.out.println("二人目の体重:");
+		double d = sc4.nextDouble();
+
 		double bmi=(b/(a*a));
 
+		double bmi2=(d/(c*c));
+
 		System.out.println(String.format("%.2f",bmi));
+		system.out.println(String.format("%.2f",bmi2));
 	}
 }
 
